723e7eab876a2cf3fe8d1b98f58502ad5d9bce57
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-08-28 17:07:41 -0500

    v5

---
+++ Datos_Number_and_String/script.js
@@ -0,0 +1,15 @@
+
+// Numero
+let variableNumero = 10
+let variableDecimal = 10.10
+
+// Texto - String
+let variableTexto='Texto'; //valor string (cadena de caracteres), 
+                        // todo lo que sea texto va entre comillas
+
+let variableTexto2="Texto";
+let variableTexto3= Texto $(variableNumero)  // texto concatenado 
+
+
+
+
---
+++ Declaraciones_y_asignaciones copy/script.js
@@ -0,0 +1,12 @@
+
+
+// OPERADORES
+
+const variable = 'Valor de la variable';  //operador de asignacion
+
+const suma = 1 + 3 // es operador aricmetico de la suma
+const resta = 1 - 3 // es operador aricmetico de la resta
+const multiplicacion = 1 * 3 // es operador aricmetico de la multiplicación
+const division = 1 / 3 // es operador aricmetico de la división
+const resto = 5 % 2 // operador aricmetco de producto
+
