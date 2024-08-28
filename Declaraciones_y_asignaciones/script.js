36e3835efca7301fc0aaa0ea77c19ae4bdaecc51
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-08-28 16:26:40 -0500

    v4

--- Declaraciones_y_asignaciones/script.js
+++ Declaraciones_y_asignaciones/script.js
@@ -1,13 +1,38 @@
-var nombreDeLaVariable = 'pedro'
-let nombreDeVariable = 'Juan'
-const nombreVariable = 'javascript' /* en primera instancia se utiliza const,
+/* var nombreDeLaVariable = 'pedro'; // no se utiliza ya que se puede redeclarar muchas veces.
+
+let nombreDeVariable = 'Juan'; // esta opcion seri al segunda opcion mas utiliada
+
+const nombreVariable = 'javascript'; /* en primera instancia se utiliza const,
                                     ya que define no se puede readignar, no se puede redeclarar, 
                                     no se pueden declarrar din valor y el scope es bloque  
                                     Ver sigiente tabla:
                                     Tabla
 
 Concepto	           | VAR       | LET	|CONST
+------------------------------------------------------
 Declaración sin valor  | Sí	       | Sí	    |No
 Scope	               | Función   |Bloque	|Bloque
 Reasignación	       | Sí	       |Sí	    |No
 Redeclaración	       | Sí	       |No	    |No     */
+
+
+
+// declarar variables al mismo tiempo
+
+/*
+Let x,y,z;
+
+x=5;y=3;z=x+y;
+
+
+let X = 5;
+let y = X; //se asigna al mismo lugar de memoria X */
+
+
+function nombreFuncion(){ 
+        const nombreDeLaVariable= 'variable dentro de funcion'
+
+};
+
+
+
