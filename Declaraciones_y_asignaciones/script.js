4204eabf8e7d2a4f1bb1539d2f1dc2d03c295bfe
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-08-28 15:57:04 -0500

    Update script.js

--- Declaraciones_y_asignaciones/script.js
+++ Declaraciones_y_asignaciones/script.js
@@ -1,1 +1,13 @@
-document.getElementById('demostracion').innerHTML= 'Este texto está en otro archivo distinto'
+var nombreDeLaVariable = 'pedro'
+let nombreDeVariable = 'Juan'
+const nombreVariable = 'javascript' /* en primera instancia se utiliza const,
+                                    ya que define no se puede readignar, no se puede redeclarar, 
+                                    no se pueden declarrar din valor y el scope es bloque  
+                                    Ver sigiente tabla:
+                                    Tabla
+
+Concepto	           | VAR       | LET	|CONST
+Declaración sin valor  | Sí	       | Sí	    |No
+Scope	               | Función   |Bloque	|Bloque
+Reasignación	       | Sí	       |Sí	    |No
+Redeclaración	       | Sí	       |No	    |No     */
