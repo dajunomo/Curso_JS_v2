282b0892ba9dfc8e90d8e0c101f7f7acf18fb4d7
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-08-29 22:01:39 -0500

    v2

---
+++ Sintaxis_de_Variables/script.js
@@ -0,0 +1,15 @@
+// sintaxis de variable
+
+
+const $variable = 'Nombre';
+const variable = 'Nombre';
+const Variable = 'Nombre';
+const _variable = 'Nombre';
+
+/*
+    Reglas
+    1) se puede iniciar con $, minuscula, mayuscula y _
+    2) no se puede iniciar con numeros pero si usarlos en el resto del nombre
+    3) no se puede usar caracteres especiales salvo $ y _
+
+*/
