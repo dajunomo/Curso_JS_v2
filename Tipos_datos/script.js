336725d56e41ccb4b3c166f2784224fd845f5a38
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-08-29 22:44:45 -0500

    v3

--- Tipos_datos/script.js
+++ Tipos_datos/script.js
@@ -1,14 +1,44 @@
-//SCOPE
+// tipos de datos
 
+// number
+let a = 5 // valor entero
+let b = 5.5 // valor flotante
 
+//string
 
-let x =5
+let c = 'cadena de texto' // valor string (texto)
 
-{
-    let y =3;
-    x += y;
-    console.log(x);
+// BOOLEANO
+let d = true;
+let e = false;
+
+//undefined
+
+let f // no se asigna valor por lo cual es indefinido
+
+//null
+
+let g = null;
+
+//object
+
+let objeto = {  // valor objeto
+// clave : valor 
 
 }
 
-console.log(x);
+//array
+
+let array= [1,2,3,4,5] //arreglos o listas de numeros
+let array2=['html','css','etc'] //arreglos o listas de palabras
+
+// date
+
+const date = new Date("08-29-2024")
+
+console.log(date);
+
+
+
+
+
