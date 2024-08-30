821854cf300448ebc5be0e3ef2d0dd6671262ea2
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-08-29 22:26:20 -0500

    v4

--- Tipos_datos/script.js
+++ Tipos_datos/script.js
@@ -5,7 +5,8 @@
 let x =5
 
 {
-    
+    let y =3;
+    x += y;
     console.log(x);
 
 }
---
+++ scope/script.js
@@ -0,0 +1,14 @@
+//SCOPE
+
+
+
+let x =5
+
+{
+    let y =3;
+    x += y;
+    console.log(x);
+
+}
+
+console.log(x);
