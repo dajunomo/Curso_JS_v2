1f7bedb85f40bb6cccb046c7dfc0d1db02d5a95f
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-09-09 12:38:24 -0500

    9/09/24_v2

---
+++ objetos/script.js
@@ -0,0 +1,17 @@
+let auto1 ={
+    marca :'Fiat',
+    modelo: 'UNO',
+    peso: 550,
+    color: 'rojo'
+}
+
+
+let auto2 ={
+    marca :'Reanult',
+    modelo: '12',
+    peso: 1550,
+    color: 'azul'
+}
+
+console.log('auto 1: ', auto1);
+console.log('auto 2: ', auto2);
