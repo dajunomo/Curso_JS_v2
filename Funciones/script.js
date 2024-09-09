fbdf6aff97eaf227400bbfb7de14bb4ebf8557a1
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-09-09 12:10:11 -0500

    9-09-2024

--- Funciones/script.js
+++ Funciones/script.js
@@ -1,1 +1,25 @@
-// FUNCIIONES
+// FUNCIONES
+let a=5;
+let b=6;
+function suma(x,y){
+    return x+y;
+}
+
+let respuesta = suma(a,b);
+
+console.log(`la respuesta de ${a} + ${b} es igual a`, respuesta)
+
+
+
+
+const temperatura = 76
+
+function transformarAC (fahrenheit){
+    return(fahrenheit-32)*(5/9)
+}
+
+const respuesta1 = transformarAC(temperatura);
+
+console.log(`La temperatura en grados Celcius es de`, parseInt(respuesta1))
+
+
