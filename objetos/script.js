60856767c0cf1fee7544c3afd23029fe2c2319e6
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-09-11 22:56:57 -0500

    Objetos:11-09-2024

--- objetos/script.js
+++ objetos/script.js
@@ -1,4 +1,4 @@
-let auto1 ={
+/* let auto1 ={
     marca :'Fiat',
     modelo: 'UNO',
     peso: 550,
@@ -15,3 +15,30 @@ let auto2 ={
 
 console.log('auto 1: ', auto1);
 console.log('auto 2: ', auto2);
+console.log('el color del auto1 es: ', auto2.color)
+
+
+auto1.marca= 'BMW'
+auto1.modelo='X5'
+
+console.log(auto1["marca"])  */
+
+const vendedor = {
+        nombre: 'pedro',
+        apellido: 'Zuñiga',
+        empresa: 'Auto SA',
+        habilidadesBlandas: ['carisma', 'puntualidad'],
+        vender: function(){
+            return "pedro ha vendido un auto"
+
+        },
+
+        nombreCompleto: function(){
+            return this.nombre + " " + this.apellido
+
+        }
+}
+
+console.log(vendedor.vender())
+console.log(vendedor.nombreCompleto())
+console.log(vendedor.habilidadesBlandas)
