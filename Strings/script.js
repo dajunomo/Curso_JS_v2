0f70b638e12d40ea0310f230eefab58202c69bd3
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-09-13 16:59:35 -0500

    Strings_2_13-09-24

--- Strings/script.js
+++ Strings/script.js
@@ -1,4 +1,23 @@
-function mostrar(){
-    document.getElementById('seleccionable').innerHTML = Date()
+//STRINGS
+/*
 
-}
+let string = '\'Cadena \t de \n texto'
+
+console.log(string)
+console.log(string.length) //lenght es para contar los caracteres
+
+let resultado = string.slice(2,5) // slice mostrar caracteres desde el 2 a 5 por ejemplo
+
+console.log(resultado)
+
+*/
+
+const texto ='La educaciones muy importante en Colombia'
+
+// const resultado = texto.replace ('Colombia', 'el mundo') //replace remplaza el string
+
+// const resultado = texto.toUpperCase ('Colombia', 'el mundo')  // toUpperCase coloca todo en mayuscula
+
+const resultado = texto.toLowerCase ('Colombia', 'el mundo')  // toUpperCase coloca todo en minuscula
+
+console.log(resultado)
