f10fbd02c549be78806f4838252507e0741658e3
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-10-02 17:02:27 -0500

    Arrays

--- Array/script.js
+++ Array/script.js
@@ -1,7 +1,14 @@
-const array = new Array(5)
+/* const array = new Array(5)
 
 array[0] = 'Cursos de HTML'
 array[1] = 'Curso de css'
 array[2] = 'Curso de JS'
 
-document.getElementById('seleccionable').innerHTML= array
+document.getElementById('seleccionable').innerHTML= array */
+
+const array=  ['zapatos', 'Ricardo','Xilofon', 'Barba']
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
