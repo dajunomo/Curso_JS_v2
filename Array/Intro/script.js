fc7115ef467166529e4054128e788b08adb73f7e
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-10-02 22:12:03 -0500

    arreglos

---
+++ Array/Intro/script.js
@@ -0,0 +1,29 @@
+/* const array = new Array(5)
+
+array[0] = 'Cursos de HTML'
+array[1] = 'Curso de css'
+array[2] = 'Curso de JS'
+
+document.getElementById('seleccionable').innerHTML= array */
+
+/* const array =  ['zapatos', 'Ricardo','Xilofon', 'Barba']
+
+let texto = '' 
+for (let index = 0; index < array.length; index++) {
+    texto += "<li>" + array[index] + '</li>'
+    
+}
+
+document.getElementById('seleccionable').innerHTML = texto  */
+
+
+
+
+const array =['HTML','CSS','JS']
+array[array.length]='REACT'
+array[array.length]='ANGULAR'
+array[9]='SQL'
+
+
+console.log(array)
+
