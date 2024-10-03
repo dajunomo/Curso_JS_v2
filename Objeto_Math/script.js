0ca293386a226abc20af43898fbb7e7dae8a7537
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-10-03 12:48:21 -0500

    Objeto_Math

---
+++ Objeto_Math/script.js
@@ -0,0 +1,28 @@
+
+Math.E              //devuelve el numero Euler              
+Math.PI             //devuelve PI
+Math.SQRT2          //devuelve la raiz cuadrada de 2   
+Math.SQRT1_2        //devuelve la raiz cuadrada de 1/2
+Math.LN2            //devuelve el logaritmo natural de 2
+Math.LN10           //devuelve el logaritmo natural de 10
+Math.LOG2E          //devuelve el logaritmo de E en base 2
+Math.LOG10E         //devuelve el logaritmo de E en base 10
+
+
+console.log(Math.E)
+console.log(Math.PI)
+console.log(Math.SQRT2)
+console.log(Math.SQRT1_2)
+console.log(Math.LN2)
+console.log(Math.LN10)
+console.log(Math.LOG2E)
+console.log(Math.LOG10E)
+console.log(Math.random()*100)
+
+
+
+
+let numero = Math.random()*100
+let redondeo = Math.round(numero)
+
+console.log(redondeo)
