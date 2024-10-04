7a546279c9aa7a3e1622e5ac11624b3393bec6fa
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-10-03 22:57:56 -0500

    Juego_adivinanza

--- Juego_Adivinanza/script.js
+++ Juego_Adivinanza/script.js
@@ -3,13 +3,18 @@ let numeroAzar= Math.floor(Math.random()*100) + 1
 
 let numeroEntrada = document.getElementById('numeroEntrada')
 let mensaje = document.getElementById('mensaje')
+let intento = document.getElementById('intento')
+let intentos = 0
 
 // esta accion se va a ejecutar cuando se toqeu el boton chequear
 function chequearResultado() {
+        intentos ++
+        intento.textContent = intentos
         let numeroIngresado = parseInt(numeroEntrada.value)
 
         if (numeroIngresado < 1 || numeroIngresado> 100 || isNaN(numeroIngresado)) {
-            mensaje.textContent = 'Por favor introduce un numero valido entre 1 y 100'
+            mensaje.textContent = 'Por favor introduce un numero valido entre 1 y 100';
+            mensaje.style.color='black';
             return
         }
 
@@ -22,7 +27,11 @@ function chequearResultado() {
             mensaje.textContent= '¡Mas Alto! El numero es mayor al que dijiste';
             mensaje.style.color= 'red';
 
+        }else {
+            mensaje.textContent= '¡Mas Bajo! El numero es menor al que dijiste';
+            mensaje.style.color= 'black';
         }
 
-
 }
+
+console.log(numeroAzar)
