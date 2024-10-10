e36a2d99be2736a184d826c3c2e799a57ad61ea4
Juan Zuñiga <100373195+dajunomo@users.noreply.github.com>
2024-10-09 22:47:08 -0500

    Bucles

---
+++ Bucles/script.js
@@ -0,0 +1,22 @@
+// let tecnologias=['html','css','js','react','angular']
+
+/*
+for (let index = 0; index < tecnologias.length; index++) {
+    console.log( tecnologias[index]);   
+}*/
+
+/*
+for(tecnologia of tecnologias) {
+    console.log(tecnologia)
+}*/
+
+/* const alumno ={
+    nombre: 'ricardo',
+    edad: 25,
+    tecnologias: ['Html','CSS']
+}
+
+for (const key in alumno) {
+   console.log(alumno[key])
+}
+*/
